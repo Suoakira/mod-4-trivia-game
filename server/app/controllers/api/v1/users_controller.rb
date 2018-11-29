21a51f3f675df20ca042b2cd24b7990e3b210b9a
github username <suoarik@gmail.com>
2018-11-29 16:19:40 +0000

    added serialzers

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -10,9 +10,20 @@ class Api::V1::UsersController < ApplicationController
         if @user
             render json: @user
         else
-            render json: {error: "Unable to Create User"}
+            render json: {error: "User not found"}, status: 400
         end
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render: @user
+        else
+            render json: {error: "Unable to create user"}, status: 400
+        end
+    end
+            
+    end 
 
     private
 
