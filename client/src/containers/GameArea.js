01ccf7095f03acda290bf88964b9f076683b9e61
joarnie <johnadamarnold@gmail.com>
2018-11-30 12:15:16 +0000

    began layout

--- client/src/containers/GameArea.js
+++ client/src/containers/GameArea.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Grid, Image } from 'semantic-ui-react'
 import GameImage from '../components/GameImage'
 import RevealTile from '../components/RevealTile'
 
@@ -8,7 +9,7 @@ import { Button, Progress } from 'semantic-ui-react'
 
 export default class GameArea extends Component {
     
-    state = { percent: 33 }
+    state = { percent: 80 }
 
   increment = () =>
     this.setState({
@@ -17,12 +18,59 @@ export default class GameArea extends Component {
 
   render() {
     return (
-      <div>
-        <Progress percent={this.state.percent} indicating />
-        <Button onClick={this.increment}>Start</Button>
-        <GameImage />
-        <RevealTile />
+        <Grid>
+    {/* main body */}
+    <Grid.Column width={13}>
+
+       <div class="ui three column grid">
+      <div class="column">
+      <RevealTile />
+
       </div>
+      <div class="column">
+      <RevealTile />
+
+      </div>
+      <div class="column">
+      <RevealTile />
+
+      </div>
+    </div>
+    <div class="ui three column grid">
+    <div class="column">
+    <RevealTile />
+
+    </div>
+    <div class="column">
+    <RevealTile />
+
+    </div>
+    <div class="column">
+    <RevealTile />
+
+    </div>
+</div>
+
+    </Grid.Column>
+
+    {/* sidebar */}
+    <Grid.Column width={3}>
+
+      <GameImage /><br></br>
+      <GameImage /><br></br>
+      <GameImage />
+
+    </Grid.Column>
+  </Grid>
+ 
     )
   }
 }
+
+    //   <div>
+    //     <Progress percent={this.state.percent} indicating />
+    //     <Button onClick={this.increment}>Start</Button>
+    //     <GameImage />
+    //     <RevealTile />
+      
+    //   </div>
